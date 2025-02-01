48154f901afda2f01fdd94add8c93a1103654757
NhiDinh <thaonhi6991@gmail.com>
2025-02-01 23:18:44 +0200

    build LocalServer

---
+++ src/main/java/com/flash_card/view_model/user_auth/LocalServer.java
@@ -0,0 +1,44 @@
+package com.flash_card.view_model.user_auth;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class LocalServer {
+    private HttpServer server;
+    private String authorizationCode;
+
+    public void start() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(3000), 0);
+        server.createContext("/", exchange -> {
+            // Extract the authorization code from the query parameters
+            String query = exchange.getRequestURI().getQuery();
+            if (query != null && query.contains("code=")) {
+                authorizationCode = query.split("code=")[1].split("&")[0];
+                String response = "You can now close this browser window.";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+
+                // Stop the server after receiving the code
+                stop();
+            }
+        });
+        server.start();
+        System.out.println("Server started, waiting for redirect...");
+    }
+
+    public void stop() {
+        if (server != null) {
+            server.stop(0);
+            System.out.println("Server stopped.");
+        }
+    }
+
+    public String getAuthorizationCode() {
+        return authorizationCode;
+    }
+}
+
