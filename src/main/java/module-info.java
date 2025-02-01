ab37af4e6591aa83563dc55846d5f1da803b2c58
NhiDinh <thaonhi6991@gmail.com>
2025-02-01 23:17:54 +0200

    implement user authentication using google api - OAuthFlow

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,29 @@ module com.flash_card {
     requires javafx.fxml;
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
-
+    requires de.saxsys.mvvmfx;
+    requires com.google.gson;
+    requires com.google.auth;
+    requires com.google.api.client;
+//    requires com.sun.net.httpserver;
+    requires jdk.httpserver;
+    requires java.desktop;
+    requires org.apache.httpcomponents.client5.httpclient5;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.client.auth;
+    requires org.checkerframework.checker.qual;
 
     opens com.flash_card to javafx.fxml;
+    opens com.flash_card.view;
     opens com.flash_card.model.datasource;
     exports com.flash_card;
+    exports com.flash_card.view;
+    exports com.flash_card.view_model;
+    opens com.flash_card.view_model to javafx.fxml;
+    exports com.flash_card.view_model.user_auth;
+    opens com.flash_card.view_model.user_auth to javafx.fxml;
+    exports com.flash_card.view.auth;
+    opens com.flash_card.view.auth;
+    exports com.flash_card.view_model.user;
+    opens com.flash_card.view_model.user to javafx.fxml;
 }
