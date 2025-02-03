26d48d2c632f61416c4b81e0e01d75bae6ba151d
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2025-02-03 20:21:18 +0200

    create flashcard page - button navigation

--- src/main/java/com/flash_card/view/createFlashcardPage/CreateFlashcardController.java
+++ src/main/java/com/flash_card/view/createFlashcardPage/CreateFlashcardController.java
@@ -7,7 +7,13 @@ import com.flash_card.model.entity.User;
 import com.flash_card.view.MenuController;
 import com.flash_card.view_model.flashcard.FlashcardViewModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateFlashcardController extends MenuController {
     @FXML
@@ -30,7 +36,8 @@ public class CreateFlashcardController extends MenuController {
         FlashcardSet flashcardSet = getCurrentFlashcardSet();
         User flashcardCreator = getCurrentUser();
         viewModel.saveFlashcard(flashcardSet, flashcardCreator);
-        System.out.println("Flashcard saved");
+        System.out.println("Flashcard saved. Create new one");
+        goToCreateFlashcardPage();
     }
 
     @FXML
@@ -38,12 +45,14 @@ public class CreateFlashcardController extends MenuController {
         FlashcardSet flashcardSet = getCurrentFlashcardSet();
         User flashcardCreator = getCurrentUser();
         viewModel.saveFlashcard(flashcardSet, flashcardCreator);
-        System.out.println("Flashcard saved");
+        System.out.println("Flashcard saved. Back to Flashcard Page");
+        goToFlashcardPage();
     }
 
     @FXML
     public void handleCancel() {
         System.out.println("Cancel");
+        goToFlashcardPage();
     }
 
     //will try to get the current flashcard set info from previous page
@@ -54,5 +63,29 @@ public class CreateFlashcardController extends MenuController {
     //will get User through AuthSessionViewModel
     private User getCurrentUser() {
         return  userDao.findById("1"); //actually should be AuthSessionViewModel.getCurrentUser().getId()
+    }
+
+    private void goToFlashcardPage() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/flash_card/fxml/flashcard.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) termField.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void goToCreateFlashcardPage() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/flash_card/fxml/create_flashcard.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) termField.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
