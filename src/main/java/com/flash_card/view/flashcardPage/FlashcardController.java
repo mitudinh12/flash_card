2431ad6b5b033d9ea999c41617fd3a5158eaef1b
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2025-02-04 22:58:49 +0200

    refactor

--- src/main/java/com/flash_card/view/flashcardPage/FlashcardController.java
+++ src/main/java/com/flash_card/view/flashcardPage/FlashcardController.java
@@ -86,25 +86,6 @@ public class FlashcardController extends ViewController {
     private void handleShare(FlashcardSet set) {
     }
 
-    @FXML
-    protected void handleLogout(ActionEvent event) {
-        authSessionViewModel.logout();
-        displayLoginPage(event);
-    }
-
-    public void displayLoginPage(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/flash_card/fxml/login.fxml"));
-            Parent root = loader.load();
-
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (Exception e) {
-            System.err.println("Error in display Login page. " + e);
-        }
-    }
-
     public void setUserName(String name) {
         userName.setText("Hi, " + name);
     }
