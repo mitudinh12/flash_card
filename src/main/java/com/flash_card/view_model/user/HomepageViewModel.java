0c9fa765ab76cd497c618c779f486e4951e4fed7
NhiDinh <thaonhi6991@gmail.com>
2025-02-08 12:02:39 +0200

    -refactor and update HomepageViewModel to integrate 2 type of flashcard set display

--- src/main/java/com/flash_card/view_model/user/HomepageViewModel.java
+++ src/main/java/com/flash_card/view_model/user/HomepageViewModel.java
@@ -1,38 +1,75 @@
 package com.flash_card.view_model.user;
 
 import com.flash_card.model.dao.FlashcardSetDao;
+import com.flash_card.model.dao.SharedSetsDao;
 import com.flash_card.model.entity.FlashcardSet;
-import com.flash_card.view_model.flashcard_set.DisplayFlashcardSetViewModel;
+import com.flash_card.model.entity.SharedSet;
+import com.flash_card.view_model.flashcard_set.OwnFlashcardSetViewModel;
+import com.flash_card.view_model.flashcard_set.SetViewModel;
+import com.flash_card.view_model.flashcard_set.SharedFlashcardSetViewModel;
+import com.flash_card.view_model.flashcard_set.SharedSetViewModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomepageViewModel {
     private final FlashcardSetDao flashcardSetDAO = FlashcardSetDao.getInstance();
-    private final ObservableList<DisplayFlashcardSetViewModel> flashcardList = FXCollections.observableArrayList();
+    private final SharedSetsDao sharedSetsDao = SharedSetsDao.getInstance();
+    private final ObservableList<OwnFlashcardSetViewModel> ownFlashcardList = FXCollections.observableArrayList();
+    private final ObservableList<SharedFlashcardSetViewModel> sharedFlashcardList = FXCollections.observableArrayList();
+    private final ObservableList<SetViewModel> flashcardList = FXCollections.observableArrayList();
+    private String userId;
 
     public HomepageViewModel(String userId) {
+        this.userId = userId;
         loadFlashcards(userId);
     }
 
     private void loadFlashcards(String userId) {
-        List<FlashcardSet> entities = flashcardSetDAO.findAllSetsByCreatorId(userId);
+        List<FlashcardSet> ownFlashcardSets = flashcardSetDAO.findAllSetsByCreatorId(userId);
+        List<SharedSet> sharedSets = sharedSetsDao.findByUserId(userId);
+        List<FlashcardSet> sharedFlashcardSets = new ArrayList<>();
+        for (SharedSet sharedSet : sharedSets) {
+            sharedFlashcardSets.add(sharedSet.getFlashcardSet());
+        }
 
-        flashcardList.clear();
-        entities.stream()
-                .map(DisplayFlashcardSetViewModel::new)
-                .forEach(flashcardList::add);
+        sharedFlashcardList.clear();
+        ownFlashcardList.clear();
+        ownFlashcardSets.stream()
+                .map(OwnFlashcardSetViewModel::new)
+                .forEach(ownFlashcardList::add);
+        sharedFlashcardSets.stream()
+                .map(SharedFlashcardSetViewModel::new)
+                .forEach(sharedFlashcardList::add);
+
+        flashcardList.addAll(ownFlashcardList);
+        flashcardList.addAll(sharedFlashcardList);
     }
 
-    public ObservableList<DisplayFlashcardSetViewModel> getFlashcardList() {
-        return flashcardList;
+    public ObservableList<OwnFlashcardSetViewModel> getOwnFlashcardList() {
+        return ownFlashcardList;
     }
 
-    public void deleteFlashcardSet( DisplayFlashcardSetViewModel viewModel) {
+    public ObservableList<SharedFlashcardSetViewModel> getSharedFlashcardList() {
+        return sharedFlashcardList;
+    }
+
+    public ObservableList<SetViewModel> getFlashcardList() {return flashcardList;}
+
+    public void deleteFlashcardSet(SetViewModel viewModel) {
         if (viewModel == null) return;
 
-        flashcardSetDAO.delete(viewModel.getSet());
-
+        if (viewModel.getType().equals("own")) {
+            flashcardSetDAO.delete(viewModel.getSet());
+            ownFlashcardList.remove(viewModel);
+        } else {
+            SharedSet sharedSet = sharedSetsDao.findBySetIdAndUserId(viewModel.getSet().getSetId(), userId);
+            sharedSetsDao.delete(sharedSet);
+            sharedFlashcardList.remove(viewModel);
+        }
         flashcardList.remove(viewModel);
+
     }
 }
