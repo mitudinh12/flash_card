331596304be01e1756325996fa6d9da9d7220a71
Tu Dinh <tu.dinh@metropolia.fi>
2025-02-10 20:19:00 +0200

    test: MariaDBConnection (Kim)

--- src/test/java/com/flash_card/model/datasource/MariaDbJpaConnectionTest.java
+++ src/test/java/com/flash_card/model/datasource/MariaDbJpaConnectionTest.java
@@ -1,5 +1,6 @@
 package com.flash_card.model.datasource;
 
+import com.flash_card.model.datasource.MariaDbJpaConnection;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,17 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MariaDbJpaConnectionTest {
 
-//    @Test
-//    public void testGetInstance() {
-//        EntityManager em = MariaDbJpaConnection.getInstance();
-//        assertNotNull(em, "EntityManager should not be null");
-//    }
-//
-//    @Test
-//    public void testEntityManagerFactorySingleton() {
-//        EntityManager em1 = MariaDbJpaConnection.getInstance();
-//        EntityManager em2 = MariaDbJpaConnection.getInstance();
-//        assertSame(em1, em2, "EntityManager instances should be the same");
-//    }
+    @BeforeAll
+    public static void setUp() {
+        // Set the system property to use the test persistence unit
+        System.setProperty("persistenceUnitName", "FlashcardMariaDbUnitTest");
+    }
 
+    @Test
+    public void testGetInstance() {
+        EntityManager em = MariaDbJpaConnection.getInstance();
+        assertNotNull(em, "EntityManager should not be null");
+    }
+
+    @Test
+    public void testEntityManagerFactorySingleton() {
+        EntityManager em1 = MariaDbJpaConnection.getInstance();
+        EntityManager em2 = MariaDbJpaConnection.getInstance();
+        assertSame(em1, em2, "EntityManager instances should be the same");
+    }
+
+    @Test
+    public void testPersistenceUnitName() {
+        System.setProperty("persistenceUnitName", "FlashcardMariaDbUnitTest");
+        EntityManager em = MariaDbJpaConnection.getInstance();
+        assertNotNull(em, "EntityManager should not be null");
+    }
 }
