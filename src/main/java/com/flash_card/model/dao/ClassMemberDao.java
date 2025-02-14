b0839eb47393865f35c8231f03161696e6915de3
NhiDinh <thaonhi6991@gmail.com>
2025-02-14 22:07:36 +0200

    -add ClassMemberDao

---
+++ src/main/java/com/flash_card/model/dao/ClassMemberDao.java
@@ -0,0 +1,68 @@
+package com.flash_card.model.dao;
+
+import com.flash_card.model.entity.ClassMember;
+import com.flash_card.model.entity.User;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class ClassMemberDao {
+    private static ClassMemberDao classMemberDao = null;
+    private EntityManager em;
+
+    private ClassMemberDao(EntityManager entityManager) {
+        this.em = entityManager;
+    }
+
+    public static ClassMemberDao getInstance(EntityManager em) {
+        if (classMemberDao == null) {
+            classMemberDao = new ClassMemberDao(em);
+        }
+        return classMemberDao;
+    }
+
+    public void persistClassMember(ClassMember classMember) {
+        try {
+            em.getTransaction().begin();
+            em.persist(classMember);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            System.err.println("Error in persisting ClassMember: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteClassMember(ClassMember classMember) {
+        try {
+            em.getTransaction().begin();
+            em.remove(classMember);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            System.err.println("Error in deleting ClassMember: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> findAllStudentByClassId(int classId) {
+        List<User> students = null;
+        try {
+            students = em.createQuery("SELECT u FROM User u JOIN ClassMember cm ON u.userId = cm.studentId WHERE cm.classroomId = :classId", User.class)
+                    .setParameter("classId", classId)
+                    .getResultList();
+        } catch (Exception e) {
+            System.err.println("Error in finding all students by class id: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return students;
+    }
+
+    public ClassMember findClassMemberById(int id) {
+        return em.find(ClassMember.class, id);
+    }
+}
