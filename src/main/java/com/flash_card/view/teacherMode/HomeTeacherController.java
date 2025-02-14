e010667bf7861015baa7bc33b692eb0efe548d3d
NhiDinh <thaonhi6991@gmail.com>
2025-02-14 16:36:16 +0200

    -add draft HomeTeacherController

---
+++ src/main/java/com/flash_card/view/teacherMode/HomeTeacherController.java
@@ -0,0 +1,56 @@
+package com.flash_card.view.teacherMode;
+
+import com.flash_card.framework.ViewController;
+import com.flash_card.view_model.entity.EntityManagerViewModel;
+import com.flash_card.view_model.user_auth.AuthSessionViewModel;
+import jakarta.persistence.EntityManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeTeacherController extends ViewController {
+    private AuthSessionViewModel authSessionViewModel = AuthSessionViewModel.getInstance();
+    EntityManager entityManager = EntityManagerViewModel.getEntityManager();
+    private int currentPage = 0;
+    private final int pageSize = 6;
+
+    @FXML
+    private Label userName;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button nextButton;
+    @FXML
+    private ImageView backIcon;
+
+    @FXML
+    private ImageView nextIcon;
+
+    @FXML
+    private VBox listClasses;
+
+    @FXML
+    private void goNext(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void goBack(ActionEvent event) {
+
+    }
+}
