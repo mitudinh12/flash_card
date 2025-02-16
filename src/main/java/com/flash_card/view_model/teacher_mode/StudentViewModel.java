32feaa2bb5897e8ee4d2ee38eb1e350448faa783
NhiDinh <thaonhi6991@gmail.com>
2025-02-16 20:27:10 +0200

    -add StudentViewModel

---
+++ src/main/java/com/flash_card/view_model/teacher_mode/StudentViewModel.java
@@ -0,0 +1,26 @@
+package com.flash_card.view_model.teacher_mode;
+
+import com.flash_card.model.entity.User;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class StudentViewModel {
+    private User student;
+    private final StringProperty studentName;
+    private final StringProperty studentEmail;
+
+    public StudentViewModel(User student, TeacherViewModel teacherViewModel) {
+        this.student = student;
+        String name = student.getFirstName() + " " + student.getLastName();
+        this.studentName = new SimpleStringProperty(name) ;
+        this.studentEmail = new SimpleStringProperty(student.getEmail());
+    }
+
+    public StringProperty getStudentName() {return studentName;}
+
+    public StringProperty getStudentEmail() {return studentEmail;}
+
+    public User getStudent() {return this.student;}
+
+    public String getStudentId() {return this.student.getUserId();}
+}
