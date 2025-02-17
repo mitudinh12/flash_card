c7e0413190df810e8ed3ca8ecc438541dbf6f51d
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2025-02-17 23:10:19 +0200

    reformat controller to controller + vm

--- src/main/java/com/flash_card/view/flashcardSet/StudyFlashcardSetController.java
+++ src/main/java/com/flash_card/view/flashcardSet/StudyFlashcardSetController.java
@@ -1,94 +1,83 @@
 package com.flash_card.view.flashcardSet;
 
-    import com.flash_card.framework.ViewController;
-    import com.flash_card.model.dao.FlashcardDao;
-    import com.flash_card.model.entity.Flashcard;
-    import com.flash_card.view.flashcard.FlashcardView;
-    import com.flash_card.view_model.entity.EntityManagerViewModel;
-    import jakarta.persistence.EntityManager;
-    import javafx.event.ActionEvent;
-    import javafx.fxml.FXML;
-    import javafx.scene.control.Label;
-    import javafx.scene.input.MouseEvent;
-    import javafx.scene.layout.StackPane;
-    import javafx.scene.layout.VBox;
-    import javafx.scene.control.Button;
+import com.flash_card.framework.ViewController;
+import com.flash_card.view.flashcard.FlashcardView;
+import com.flash_card.view_model.flashcard_set.StudyFlashcardSetViewModel;
+import com.flash_card.view_model.entity.EntityManagerViewModel;
+import jakarta.persistence.EntityManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
-    import java.util.List;
+public class StudyFlashcardSetController extends ViewController {
+    @FXML
+    public Label index;
+    @FXML
+    public Label total;
+    @FXML
+    public Label setName;
+    @FXML
+    public StackPane backIcon;
+    @FXML
+    public StackPane nextIcon;
+    @FXML
+    private VBox flashcardContainer;
 
-    public class StudyFlashcardSetController extends ViewController {
-        @FXML
-        public Label index;
-        @FXML
-        public Label total;
-        @FXML
-        public Label setName;
-        @FXML
-        public StackPane backIcon;
-        @FXML
-        public StackPane nextIcon;
-        @FXML
-        private VBox flashcardContainer;
+    private final EntityManager entityManager = EntityManagerViewModel.getEntityManager();
+    private final StudyFlashcardSetViewModel viewModel = new StudyFlashcardSetViewModel(entityManager);
 
-        private List<Flashcard> flashcards;
-        private int currentIndex = 0;
-        private EntityManager entityManager = EntityManagerViewModel.getEntityManager();;
-        private FlashcardDao flashcardDao = FlashcardDao.getInstance(entityManager);
-
-        @FXML
-        public void initialize() {
-        }
-
-        public void setFlashcardSet(int setId, String setName) {
-            flashcards = flashcardDao.findBySetId(setId);
-            this.setName.setText(setName);
-            this.total.setText(String.valueOf(flashcards.size()));
-            showFlashcard(currentIndex);
-        }
-
-        private void showFlashcard(int index) {
-            flashcardContainer.getChildren().clear();
-            Flashcard flashcard = flashcards.get(index);
-            FlashcardView flashcardView = new FlashcardView(flashcard.getTerm(), flashcard.getDefinition());
-            flashcardContainer.getChildren().add(flashcardView);
-            this.index.setText(String.valueOf(index + 1));
-            updateButtonStates();
-        }
-
-
-        private void updateButtonStates() {
-            backIcon.setVisible(currentIndex != 0);
-            nextIcon.setVisible(currentIndex != flashcards.size() - 1);
-        }
-
-        @FXML
-        public void handelEasy(ActionEvent actionEvent) {
-        }
-
-        @FXML
-        public void handleHard(ActionEvent actionEvent) {
-        }
-
-        @FXML
-        public void handleReset(MouseEvent mouseEvent) {
-        }
-
-        @FXML
-        public void handleShuffle(MouseEvent mouseEvent) {
-        }
-
-        @FXML
-        public void handleNext(MouseEvent mouseEvent) {
-            if (currentIndex < flashcards.size() - 1) {
-                currentIndex++;
-                showFlashcard(currentIndex);
-            }
-        }
-        @FXML
-        public void handleBack(MouseEvent mouseEvent) {
-            if (currentIndex > 0) {
-                currentIndex--;
-                showFlashcard(currentIndex);
-            }
-        }
+    @FXML
+    public void initialize() {
+        setName.textProperty().bind(viewModel.setNameProperty());
+        total.textProperty().bind(viewModel.totalProperty());
+        index.textProperty().bind(viewModel.currentIndexProperty().add(1).asString());
     }
+
+    public void setFlashcardSet(int setId, String setName) {
+        viewModel.loadFlashcards(setId, setName);
+        showFlashcard();
+    }
+
+    private void showFlashcard() {
+        flashcardContainer.getChildren().clear();
+        FlashcardView flashcardView = new FlashcardView(viewModel.getCurrentFlashcard().getTerm(), viewModel.getCurrentFlashcard().getDefinition());
+        flashcardContainer.getChildren().add(flashcardView);
+        updateButtonStates();
+    }
+
+    private void updateButtonStates() {
+        backIcon.setVisible(viewModel.currentIndexProperty().get() != 0);
+        nextIcon.setVisible(viewModel.currentIndexProperty().get() != Integer.parseInt(viewModel.totalProperty().get()) - 1);
+    }
+
+    @FXML
+    public void handleNext(MouseEvent mouseEvent) {
+        viewModel.nextFlashcard();
+        showFlashcard();
+    }
+
+    @FXML
+    public void handleBack(MouseEvent mouseEvent) {
+        viewModel.previousFlashcard();
+        showFlashcard();
+    }
+
+    @FXML
+    public void handelEasy(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleHard(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleReset(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void handleShuffle(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/com/flash_card/view_model/flashcard_set/StudyFlashcardSetViewModel.java
@@ -0,0 +1,57 @@
+package com.flash_card.view_model.flashcard_set;
+
+import com.flash_card.model.dao.FlashcardDao;
+import com.flash_card.model.entity.Flashcard;
+import jakarta.persistence.EntityManager;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.util.List;
+
+public class StudyFlashcardSetViewModel {
+    private final FlashcardDao flashcardDao;
+    private List<Flashcard> flashcards;
+    private final IntegerProperty currentIndex = new SimpleIntegerProperty(0);
+    private final StringProperty setName = new SimpleStringProperty();
+    private final StringProperty total = new SimpleStringProperty();
+
+    public StudyFlashcardSetViewModel(EntityManager entityManager) {
+        flashcardDao = FlashcardDao.getInstance(entityManager);
+    }
+
+    public void loadFlashcards(int setId, String setName) {
+        flashcards = flashcardDao.findBySetId(setId);
+        this.setName.set(setName);
+        this.total.set(String.valueOf(flashcards.size()));
+    }
+
+    public Flashcard getCurrentFlashcard() {
+        return flashcards.get(currentIndex.get());
+    }
+
+    public void nextFlashcard() {
+        if (currentIndex.get() < flashcards.size() - 1) {
+            currentIndex.set(currentIndex.get() + 1);
+        }
+    }
+
+    public void previousFlashcard() {
+        if (currentIndex.get() > 0) {
+            currentIndex.set(currentIndex.get() - 1);
+        }
+    }
+
+    public IntegerProperty currentIndexProperty() {
+        return currentIndex;
+    }
+
+    public StringProperty setNameProperty() {
+        return setName;
+    }
+
+    public StringProperty totalProperty() {
+        return total;
+    }
+}
