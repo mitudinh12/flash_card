734bf6eb636eef38bf0edba87a718b5a395e28e3
NhiDinh <thaonhi6991@gmail.com>
2025-02-24 17:30:31 +0200

    -update ClassroomDao and add ClassroomDaoTest

--- src/test/java/com/flash_card/model/dao/ClassroomDaoTest.java
+++ src/test/java/com/flash_card/model/dao/ClassroomDaoTest.java
@@ -5,13 +5,13 @@ import com.flash_card.model.entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 class ClassroomDaoTest {
     private Classroom testClassroom;
@@ -39,17 +39,22 @@ class ClassroomDaoTest {
         classroomDao.deleteClass(testClassroom);
         testClassroom = null;
         userDao.delete(testTeacher);
+
     }
 
     @Test
+    @Order(1)
     void testPersistClass() {
         classroomDao.persistClass(classroom);
         testClassroom = classroomDao.findClassById(classroom.getClassroomId());
         assertNotNull(testClassroom, "Persisted classroom should be found");
         assertEquals(className, testClassroom.getClassroomName(), "Class name should match");
+        Classroom invalidClassroom = null;
+        assertFalse(classroomDao.persistClass(invalidClassroom), "Persisting classroom should return false when an exception occurs");
     }
 
     @Test
+    @Order(2)
     void testDeleteClass() {
         classroomDao.persistClass(classroom);
         testClassroom = classroomDao.findClassById(classroom.getClassroomId());
@@ -57,9 +62,11 @@ class ClassroomDaoTest {
         classroomDao.deleteClass(testClassroom);
         testClassroom = classroomDao.findClassById(classroom.getClassroomId());
         assertNull(testClassroom, "Deleted classroom should not be found");
+        assertFalse(classroomDao.deleteClass(testClassroom), "Deleting the non exist classroom should return false");
     }
 
     @Test
+    @Order(3)
     void testUpdateClass() {
         classroomDao.persistClass(classroom);
         testClassroom = classroomDao.findClassById(classroom.getClassroomId());
@@ -69,17 +76,22 @@ class ClassroomDaoTest {
         classroomDao.updateClass(testClassroom);
         testClassroom = classroomDao.findClassById(classroom.getClassroomId());
         assertEquals(updatedClassName, testClassroom.getClassroomName(), "Class name should be updated");
+        Classroom invalidClassroom = null;
+        assertFalse(classroomDao.updateClass(invalidClassroom), "Updating the non exist classroom should return false");
     }
 
     @Test
+    @Order(4)
     void testFindClassById() {
         classroomDao.persistClass(classroom);
         testClassroom = classroomDao.findClassById(classroom.getClassroomId());
         assertNotNull(testClassroom, "Persisted classroom should be found");
         assertEquals(className, testClassroom.getClassroomName(), "Class name should match");
+        assertNull(classroomDao.findClassById(0), "Finding the non exist classroom should return null");
     }
 
     @Test
+    @Order(5)
     void testFindAllClassByTeacherId() {
         classroomDao.persistClass(classroom);
         List<Classroom> classrooms = classroomDao.findAllClassByTeacherId(testTeacher.getUserId());
@@ -87,5 +99,4 @@ class ClassroomDaoTest {
         assertTrue(classrooms.contains(classroom), "Persisted classroom should be found");
         assertEquals(className, testClassroom.getClassroomName(), "Class name should match");
     }
-
 }
