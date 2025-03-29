669e53d00def149aec5790e88588b1322892e27a
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2025-03-29 22:44:03 +0200

    localized student quiz result

--- src/main/java/com/flash_card/view/studentMode/StudentQuizResultController.java
+++ src/main/java/com/flash_card/view/studentMode/StudentQuizResultController.java
@@ -2,9 +2,16 @@ package com.flash_card.view.studentMode;
 
 import com.flash_card.view.flashcardSet.QuizResultController;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class StudentQuizResultController extends QuizResultController {
+    @Override
+    public void initialize() {
+        setReloadFxml("/com/flash_card/fxml/student-quiz-result.fxml");
+        setResultView(quizSession.getQuizId());
+    }
 
+    @FXML
     public void goToStudentHome(ActionEvent event) {
         goToPage("/com/flash_card/fxml/student-class.fxml", setNameLabel.getScene());
     }
