99ce198c6b69d9569d17a919516051b64feb83d3
Tu Dinh <tu.dinh@metropolia.fi>
2025-04-09 09:31:15 +0300

    refactor: vm/entity

--- src/main/java/com/flash_card/view_model/entity/EntityManagerViewModel.java
+++ src/main/java/com/flash_card/view_model/entity/EntityManagerViewModel.java
@@ -2,15 +2,31 @@ package com.flash_card.view_model.entity;
 
 import com.flash_card.model.datasource.MariaDbJpaConnection;
 import jakarta.persistence.EntityManager;
-
+/**
+ * ViewModel class for managing the {@link EntityManager} instance.
+ * Provides methods to retrieve and set the {@link EntityManager} used
+ * for database operations.
+ */
 public class EntityManagerViewModel {
+    /**
+     * The {@link EntityManager} instance used for database operations.
+     */
     private EntityManager entityManager;
-
+    /**
+     * Retrieves the singleton {@link EntityManager} instance from
+     * {@link MariaDbJpaConnection}.
+     *
+     * @return the singleton {@link EntityManager} instance
+     */
     public static EntityManager getEntityManager() {
         return MariaDbJpaConnection.getInstance();
     }
-
-    public void setEntityManager(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    /**
+     * Sets the {@link EntityManager} instance for this ViewModel.
+     *
+     * @param entityManagerParam the {@link EntityManager} instance to set
+     */
+    public void setEntityManager(final EntityManager entityManagerParam) {
+        this.entityManager = entityManagerParam;
     }
 }
